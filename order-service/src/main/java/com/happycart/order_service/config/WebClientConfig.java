dca0efbcdde740e427ff1ada72414160a5856916
Sainath Challa <sainath.challa@fedex.com>
2026-03-07 23:12:58 +0530

    Add API Gateway module and update configs

--- order-service/src/main/java/com/happycart/order_service/config/WebClientConfig.java
+++ order-service/src/main/java/com/happycart/order_service/config/WebClientConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    // Use of LoadBalanced Annotation -> Client side load balancing, and It will intercept the requests from order service and finds the endpoint of inventory service.
+    // If not used, then inventory-service(used in url to hit), will be intercepted by DNS, and we won't find it.
     @Bean
     @LoadBalanced
     public WebClient.Builder getWebClientBuilder() {
