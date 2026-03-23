035c2355e21efef15d49d2d46e7ed1a4eb360e7f
Sainath Challa <sainath.challa@fedex.com>
2026-03-23 19:05:34 +0530

    Add Dockerfiles and docker-compose configs

--- api-gateway/src/main/java/com/happycart/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/happycart/api_gateway/config/SecurityConfig.java
@@ -16,8 +16,8 @@ public class SecurityConfig {
                 .csrf(csrfSpec -> csrfSpec.disable())
                 .authorizeExchange(authorizeExchangeSpec -> {
                     authorizeExchangeSpec
-//                            .pathMatchers("/eureka/**")
-//                            .permitAll()
+                            .pathMatchers("/eureka/**")
+                            .permitAll()
                             .anyExchange()
                             .authenticated();
                 })
